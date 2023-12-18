da4c0cb62648e81d34eb77bb57edf0f8678520a4
Artemskyba <131376392+Artemskyba@users.noreply.github.com>
2023-12-18 15:08:37 +0000

    Deploying to gh-pages from @ Artemskyba/goit-react-hw-08-phonebook@c047432db61eec23b1465f8280a65e5b8cc9aa17 🚀

---
+++ static/js/114.e41d826b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[114],{6114:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(3329);function o(){return(0,r.jsx)("div",{children:(0,r.jsx)("b",{children:"Welcome to PhoneBook!"})})}}}]);
+//# sourceMappingURL=114.e41d826b.chunk.js.map
